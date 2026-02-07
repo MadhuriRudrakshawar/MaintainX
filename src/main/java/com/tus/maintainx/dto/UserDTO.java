f5f8bc6015ffd1450183f364b23b0e5d2c01cbe2
Madhuri <a00335636@student.tus.ie>
2026-02-07 22:53:15 +0000

    User Entity, UserDto added

---
+++ src/main/java/com/tus/maintainx/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package com.tus.maintainx.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+@Builder
+public class UserDTO {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
+
---
+++ src/main/java/com/tus/maintainx/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.tus.maintainx.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,  length = 50)
+    private String username;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+}
