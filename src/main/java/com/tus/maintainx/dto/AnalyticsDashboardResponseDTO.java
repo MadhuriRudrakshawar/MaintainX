67de1e4dca777895bbb25503c2b7b3f6a3ab5765
Madhuri <a00335636@student.tus.ie>
2026-03-07 13:39:30 +0000

    Analytics entity, request responce DTO added

---
+++ src/main/java/com/tus/maintainx/dto/AnalyticsDashboardResponseDTO.java
@@ -0,0 +1,20 @@
+package com.tus.maintainx.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Map;
+
+@Data
+@Builder
+public class AnalyticsDashboardResponseDTO {
+    private Map<String, Long> maintenanceStatusCounts;
+    private Map<String, Long> windowsByDate;
+    private Map<String, Long> bookedSlotsByHour;
+    private Map<String, Long> bookedSlotsByDateHour;
+    private Map<String, Long> elementsByType;
+    private Map<String, Long> elementsByStatus;
+    private List<ApprovedWindowTimelineDTO> approvedWindowTimeline;
+    private Map<String, Long> topImpactedElements;
+}
